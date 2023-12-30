dc55613e12bc26d3b7cd5b96c571353c4b84f39d
gnd10137 <154660760+gnd10137@users.noreply.github.com>
2023-12-30 16:50:33 +0900

    2023/12/30

---
+++ 자유롭게.py
@@ -0,0 +1,62 @@
+##s1='hello\nworld!'
+##s2='Python is fun!'
+##print(s1)
+##print(s2)
+##list1=[]
+##list2=[1,2,3]
+##list3=['a','b','c']
+##list4=['hello', 'world']
+##list5=[1,2,'a','b','python']
+##list6=[1,2,['a','b','c'], 3]
+##print(list1)
+##print(list2)
+##print(list3)
+##print(list4)
+##print(list5)
+##print(list6)
+##a='python'
+##b='is fun'
+##print(a+b)
+##print(a*2)
+##a=[1,2,3]
+##b=[4,5,6]
+##print(a+b)
+##print(a*3)
+##a = '*'
+##print(a*10)
+##a='*'
+##print('%c'%a)
+##print('%s'%(a*3))
+##print('%s'%(a*6))
+##print('%s'%(a*2))
+##print('%c'%a)
+##a='Carpe Diem!'
+##print(a[6:11])
+##a='Carpe Diem!'
+##print(a[-5:-1])
+##a='Carpe Diem!'
+##print(a[0:5])
+##a=[1,2,'a','b',3,4,'c']
+##print(a[5])
+##print(a[0:2])
+##print(a[4:-1])
+##print(a[0]+a[1])
+##print(a[2]+a[3]+a[6])
+##a=[10,20,30,40,50]
+##a[0]='a'
+##print(a)
+##a[1:3]='b'
+##print(a)
+##a='I Love You'
+##print(a.count('o'))
+##print(a.find('Y'), a.index('Y'))
+##print('.'.join(a))
+##print(a.split())
+##print(a.replace('Love', 'Like'))
+##print(a.upper(), a. lower())
+##a='Hello World'
+##print(a.count('l'))
+##b='python'
+##print(b.find('p'))
+c='oh!my!god'
+print(c.split('!'))
