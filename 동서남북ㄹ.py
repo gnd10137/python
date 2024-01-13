0955a0aa4fcd35c332d14f49414b923d9512e176
gnd10137 <154660760+gnd10137@users.noreply.github.com>
2024-01-13 16:48:31 +0900

    2024/1/13

---
+++ 동서남북ㄹ.py
@@ -0,0 +1,49 @@
+##x=10
+##y=50
+##print('x:%d,y:%d'%(x,y))
+##print('x와 y는 같다?',x==y)
+##print('x와 y는 다르다?',x!=y)
+##print('x는 y보다 크다?',x>y)
+##print('x는 y보다 작다?',x<y)
+##print('x는 y보다 크거나 같다?',x>=y)
+##print('x는 y보다 작거나 같다?',x<=y)
+##score=80
+##if score>=60 and score<=100:
+##     print('Pass')
+##else:
+##     print('Fail')
+##a=int(input())
+##b=int(input())
+##c=int(input())
+##print(a>=140 and b>=140 and c>=140)
+##score=int(input('score:'))
+##if score>=60:
+##     print('Pass')
+##else:
+##     print('Fail')
+##a=int(input('정수 입력'))
+##if (a%4==0):
+##     print('4의 배수입니다.')
+##else:
+##     print('4의 배수가 아닙니다.')
+##score=72
+##if score>=90:
+##     print('A')
+##elif score>=60:
+##     print('B')
+##else:
+##     print('C')
+##a=input('성별을 입력하세요')
+##if (a=='M'):
+##     print('Man')
+##elif (a=='W'):
+##     print('Woman')
+##else:
+##     print('What?')
+##a=int(input('월을 입력하세요'))
+##if (a>=1 and a<=6):
+##     print('first half')
+##elif(a>6 and a<13):
+##     print('second half')
+##else:
+##     print('What?')
