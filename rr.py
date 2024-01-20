be141f7f0774eb3249a974e880cbfd247087f7b2
gnd10137 <154660760+gnd10137@users.noreply.github.com>
2024-01-20 12:57:38 +0900

    2024/1/20

---
+++ rr.py
@@ -0,0 +1,28 @@
+##a=input('성별을 입력하세요')
+##if (a=='M'):
+##     print('Man')
+##elif (a=='W'):
+##     print('Woman')
+##else:
+##     print('What?')
+##a=int(input('월을 입력하세요'))
+##if (a>=1 and a<=6):
+##     print('first half')
+##elif(a>6 and a<13):
+##     print('second half')
+##else:
+##     print('What?')
+##a=int(input('숫자를 입력하세요'))
+##if (a==1):
+##    print('Burgers are not available.')
+##elif (a==2 or a==3):
+##    print('What would you like to drink?')
+##elif (a==4):
+##    print('I like coke, too.')
+##elif (a==5):
+##    print('Would you like hot or cold?')
+##a=int(input())
+##if (a%2==0):
+##    print('EVEN')
+##else:
+##    print('ODD')
