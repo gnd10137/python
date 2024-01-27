45b246d57e6c4fa21d667c9498a23a780961981a
gnd10137 <154660760+gnd10137@users.noreply.github.com>
2024-01-27 16:54:37 +0900

    2024/1/27

---
+++ 틱톡타이머.py
@@ -0,0 +1,57 @@
+##i=1
+##while i<=10:
+##     print(i,end=' ')
+##     i+=1
+
+##sum=0
+##i=1
+##while i <=100:
+##     sum+=i
+##     i+=1
+##print(sum)
+
+##i=0
+##while i<10:
+##     i+=1
+##     if i %2==0:
+##          continue
+##     print(i,end=' ')
+
+##while True:
+##     ans=input('Shall we close?(y/n)')
+##     if ans=='y':
+##          print('The end')
+##          break
+
+##ans=' '
+##while ans!='Yes':
+##     ans=input('Are you ready?')
+##print('going out')
+
+##stamp=0
+##while stamp<10:
+##     stamp+=1
+##     print('스탬프 %d개 적립'%stamp)
+##     if stamp==10:
+##          print('무료 음료 쿠폰 1개 증정')
+
+##i=1
+##while i<=100:
+##     print(i,end=' ')
+##     i+=1
+
+##sum=0
+##i=1
+##a=int(input())
+##while i <=a:
+##     sum+=i
+##     i+=1
+##print(sum)
+
+i=0
+while i<5:
+     a=int(input('정수 입력:'))
+     if a%5!=0:
+          print(a)
+     else:
+          continue
