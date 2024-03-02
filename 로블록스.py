dce41349414e1c05e83a9f8642d77fa7de4e9864
gnd10137 <154660760+gnd10137@users.noreply.github.com>
2024-03-02 16:51:24 +0900

    2024/3/2

---
+++ 로블록스.py
@@ -0,0 +1,88 @@
+##def add(x,y):
+##     s=x+y
+##     return s
+##result=add(10,20)+add(20,30)
+##print(result)
+
+##def f(n):
+##     n=20
+##     return n
+##n=10
+##f(n)
+##print(n)
+##n=f(n)
+##print(n)
+
+##def get_max(x,y):
+##     if x>y:
+##          return x
+##     else:
+##          return y
+##x=int(input('정수를 입력하세요'))
+##y=int(input('정수를 입력하세요'))
+##n=get_max(x,y)
+##print(n)
+
+##def plus(a):
+##     if a>0:
+##          return True
+##     else:
+##          return False
+##n=int(input('정수를 입력하세요.'))
+##n=plus(n)
+##print(n)
+
+##def get_sum(n):
+##     s=0
+##     for i in range(1,n+1):
+##          s+=i
+##     return s
+##n=int(input())
+##print('1부터 %d까지의 합은 %d입니다.'%(n,get_sum(n)))
+
+##for i in range(1,11):
+##     print(i,end=' ')
+
+##a=int(input('정수를 입력하세요'))
+##for i in range(1,a+1):
+##     print(i,end=' ')
+
+##a=int(input('정수를 입력하세요'))
+##for i in range(a,51):
+##     print(i,end=' ')
+
+##a=int(input('정수를 입력하세요'))
+##b=int(input('정수를 입력하세요'))
+##for i in range(a,b+1):
+##     print(i,end=' ')
+
+##def get_sum(a,b):
+##     s=0
+##     for i in range(a,b+1):
+##          s+=i
+##     return s
+##
+##a=int(input())
+##b=int(input())
+##print(get_sum(a,b))
+
+##def area(a,b):
+##     return a*b
+##a=int(input('가로입력'))
+##b=int(input('세로입력'))
+##print(area(a,b))
+
+##def area(a):
+##     s=3.14*a*a
+##     return s
+##n=int(input('반지름'))
+##print(area(n))
+
+##def number(n):
+##     b=n%2
+##     if b==1:
+##          return'odd'
+##     else:
+##          return 'even'
+##n=int(input('정수입력'))
+##print(number(n))
