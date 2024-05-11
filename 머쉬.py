6588b02a7ec590567387b06c8ce0f276b47ed14c
gnd10137 <154660760+gnd10137@users.noreply.github.com>
2024-05-11 12:49:51 +0900

    2024/5/11

--- 머쉬.py
+++ 머쉬.py
@@ -1,52 +1,88 @@
-##from random import*
-##a=randint(1,100)
-##while True:
-##     b=int(input('정수입력'))
-##     if a>b:
-##          print('up')
-##     elif b>a:
-##          print('down')
-##     elif a==b:
-##          print('correct')
-##          break
+##from tkinter import*
+##
+##win=Tk()
+##canvas=Canvas(win,bg='white',width=300,height=300)
+##canvas.pack()
+##win.mainloop()
 
-##a=int(input('정수입력'))
-##for i in range(1,a+1):
+##from tkinter import*
+##
+##win=Tk()
+##canvas=Canvas(win,bg='yellow',width=100,height=100)
+##canvas.pack()
+##win.mainloop()
 
-##a=int(input('정수입력'))
-##if a%2==0:
-##     print('짝수')
-##else:
-##     print('홀수')
+##from tkinter import*
+##
+##win=Tk()
+##canvas=Canvas(win,bg='white',width=500,height=100)
+##canvas.create_line(0,50,500,50,fill='blue',width=5)
+##canvas.pack(fill='both',expand=True)
+##win.mainloop()
 
-##a=int(input('정수입력'))
-##i=0
-##sum=0
-##while i<=a:
-##     if i%3==0:
-##          sum+=i
-##     i+=1
-##print(sum)
+##from tkinter import*
+##
+##win=Tk()
+##canvas=Canvas(win,bg='white',width=500,height=100)
+##canvas.create_line(0,30,300,30,fill='green',width=2)
+##canvas.pack()
+##win.mainloop()
 
-##for i in range(1,101):
-##     if i%2==0:
-##          print(i)
+##from tkinter import*
+##
+##win=Tk()
+##canvas=Canvas(win,bg='white',width=300,height=300)
+##canvas.create_line(0,30,300,30,fill='red',width=5)
+##canvas.pack()
+##win.mainloop()
 
-##a=int(input('a:'))
-##b=int(input('b:'))
-##print('%d+%d=%d'%(a,b,a+b))
-##print('%d-%d=%d'%(a,b,a-b))
-##print('%d*%d=%d'%(a,b,a*b))
-##print('%d//%d=%d'%(a,b,a//b))
+##from tkinter import*
+##
+##win=Tk()
+##canvas=Canvas(win,bg='white',width=500,height=100)
+##canvas.pack()
+##
+##x1,y1=0,0
+##x2,y2=500,0
+##
+##for i in range(3):
+##    y1+=30
+##    y2=y1
+##    canvas.create_line(x1,y1,x2,y2,fill='red',width=3)
+##
+##win.mainloop()
 
-##a=int(input('height:'))
-##b=int(input('width:'))
-##print('area:%.1f'%(a*b/2))
+##from tkinter import*
+##def paint(event):
+##    x1,y1=event.x,event.y
+##    x2,y2=x1+5,y1+5
+##    canvas.create_line(x1,y1,x2,y2,width=3)
+##
+##win=Tk()
+##canvas=Canvas(win,bg='white',width=500,height=200)
+##canvas.pack()
+##
+##win.bind('<B1-Motion>',paint)
+##win.mainloop()
 
-L=int(input('L 판매 개수:'))
-M=int(input('M 판매 개수:'))
-S=int(input('S 판매 개수:'))
-sum=2500*L
-sum=1000*M
-sum=500*S
-print('오늘의 총 매출은 %d원입니다.'%sum)
+from tkinter import*
+pen_color='black'
+
+def paint(event):
+    global pen_color
+    x1,y1=event.x,event.y
+    x2,y2=x1+5,y1+5
+    canvas.create_line(x1,y1,x2,y2,width=3,fill=pen_color)
+
+def change_color():
+    global pen_color
+    pen_color='red'
+
+win=Tk()
+canvas=Canvas(win,bg='white',width=500,height=200)
+btn=Button(win,text='red',command=change_color)
+
+canvas.pack()
+btn.pack()
+win.bind('<B1-Motion>',paint)
+win.mainloop()
