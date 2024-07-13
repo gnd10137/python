53a8ba22c5824e1ca52b95143399d49c073415d7
gnd10137 <154660760+gnd10137@users.noreply.github.com>
2024-07-13 12:50:39 +0900

    2024/7/13

---
+++ 에이스.cpp
@@ -0,0 +1,115 @@
+//#include <stdio.h>
+//int main()
+//{
+//	int a,b;
+//	a=23;
+//	b=24;
+//	printf("%d\n",a>b);
+//	printf("%d\n",a<b);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("%d\n",10>5) && (8<3);
+//	printf("%d\n",10>5) || (8<3);
+//	printf("%d\n", !(10>8));
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int res;
+//	
+//	res=0 && 0;
+//	printf("%d\n",res);
+//	res=0 && 1;
+//	printf("%d\n",res);
+//	res=1 && 0;
+//	printf("%d\n",res);
+//	res=1 && 1;
+//	printf("%d\n",res);
+//	
+//	res= 0 || 0;
+//	printf("%d\n",res);
+//	res= 0 || 1;
+//	printf("%d\n",res);
+//	res= 1 || 0;
+//	printf("%d\n",res);
+//	res= 1 || 1;
+//	printf("%d\n", !res);
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a=20;
+//	
+//	a = a + 10;
+//	printf("a = %d\n", a);
+//	a = a * 10;
+//	printf("a = %d\n", a);
+//	a = a - 10;
+//	printf("a = %d\n", a);
+//	a = a / 10;
+//	printf("a = %d\n", a);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a,b;
+//	a = 20;
+//	b = 5;
+//	
+//	a+=b;
+//	printf("a = %d\n", a);
+//	a*=b;
+//	printf("a = %d\n", a);
+//	a-=b;
+//	printf("a = %d\n", a);
+//	a/=b;
+//	printf("a = %d\n", a);
+//	a%=b;
+//	printf("a = %d\n", a);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a=10;
+//	
+//	a++;
+//	a++;
+//	printf("a=%d\n",a);
+//	++a;
+//	++a;
+//	printf("a=%d\n",a);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a=10;
+//	a--;
+//	printf("a=%d\n",a);
+//	--a;
+//	printf("a=%d\n",a);
+//}
+
+#include <stdio.h>
+//int main()
+//{
+//	int a,b;
+//	
+//	a=10;
+//	b=a++;
+//	print("a=%d b=%d\n",a,b);
+//	b=++a;
+//	print("a=%d b=%d\n",a,b);
+//	
+//	a=20;
+//	b=a--;
+//	print("a=%d b=%d\n",a,b);
+//	b=--a;
+//	print("a=%d b=%d\n",a,b);
+//}
